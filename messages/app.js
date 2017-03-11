45ac5d56d2fdaf3f37d28a40ceb9dd49e21d7b59
vinayaksharma <vinayaksharma05@gmail.com>
2017-03-10 19:04:15 -0500

    DB

---
+++ messages/app.js
@@ -0,0 +1,46 @@
+"use strict";
+
+var documentClient = require("documentdb").DocumentClient;
+var config = require("./config");
+var url = require('url');
+var client = new documentClient(config.endpoint, { "masterKey": config.primaryKey });
+
+
+var HttpStatusCodes = { NOTFOUND: 404 };
+var databaseUrl = `dbs/${config.database.id}`;
+var collectionUrl = `${databaseUrl}/colls/${config.collection.id}`;
+
+function getDatabase() {
+    console.log(`Getting database:\n${config.database.id}\n`);
+
+    return new Promise((resolve, reject) => {
+        client.readDatabase(databaseUrl, (err, result) => {
+            if (err) {
+                if (err.code == HttpStatusCodes.NOTFOUND) {
+                    client.createDatabase(config.database, (err, created) => {
+                        if (err) reject(err)
+                        else resolve(created);
+                    });
+                } else {
+                    reject(err);
+                }
+            } else {
+                resolve(result);
+            }
+        });
+    });
+}
+
+// ADD THIS PART TO YOUR CODE
+function exit(message) {
+    console.log(message);
+    console.log('Press any key to exit');
+    process.stdin.setRawMode(true);
+    process.stdin.resume();
+    process.stdin.on('data', process.exit.bind(process, 0));
+}
+
+getDatabase()
+.then(() => { exit(`Completed successfully`); })
+.catch((error) => { exit(`Completed with error ${JSON.stringify(error)}`) });
+
---
+++ messages/config.js
@@ -0,0 +1,74 @@
+
+var config = {}
+
+config.endpoint = "https://waterlit.documents.azure.com:443/";
+config.primaryKey = "1TvDXekns4CAuRd9d2Gs9JAmV9JICKposyityz9xo7NdT01OD0b39ni8Dv053djFDAjFDhPq29Khh0YneBAjnA==";
+
+
+
+config.database = {
+    "id": "FamilyDB"
+};
+
+config.collection = {
+    "id": "FamilyColl"
+};
+
+config.documents = {
+    "Andersen": {
+        "id": "Anderson.1",
+        "lastName": "Andersen",
+        "parents": [{
+            "firstName": "Thomas"
+        }, {
+                "firstName": "Mary Kay"
+            }],
+        "children": [{
+            "firstName": "Henriette Thaulow",
+            "gender": "female",
+            "grade": 5,
+            "pets": [{
+                "givenName": "Fluffy"
+            }]
+        }],
+        "address": {
+            "state": "WA",
+            "county": "King",
+            "city": "Seattle"
+        }
+    },
+    "Wakefield": {
+        "id": "Wakefield.7",
+        "parents": [{
+            "familyName": "Wakefield",
+            "firstName": "Robin"
+        }, {
+                "familyName": "Miller",
+                "firstName": "Ben"
+            }],
+        "children": [{
+            "familyName": "Merriam",
+            "firstName": "Jesse",
+            "gender": "female",
+            "grade": 8,
+            "pets": [{
+                "givenName": "Goofy"
+            }, {
+                    "givenName": "Shadow"
+                }]
+        }, {
+                "familyName": "Miller",
+                "firstName": "Lisa",
+                "gender": "female",
+                "grade": 1
+            }],
+        "address": {
+            "state": "NY",
+            "county": "Manhattan",
+            "city": "NY"
+        },
+        "isRegistered": false
+    }
+};
+
+module.exports = config;
